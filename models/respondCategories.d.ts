3fe143d0f62046b7a19f38cce69bf61c90852da3
Stratis Christodoulou <stratis.vip@gmail.com>
2019-12-21 12:08:41 +0200

    Delete respondCategories.d.ts

--- models/respondCategories.d.ts
+++
@@ -1,7 +0,0 @@
-import GeneralRespond from '../models/general/general-respond';
-import { ICategory } from '../models/types';
-declare class RespondCategories extends GeneralRespond {
-    data: ICategory[];
-    constructor();
-}
-export default RespondCategories;
