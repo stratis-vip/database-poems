4e1868dd95ffbac2c93a3da1c929d44b46411952
Stratis Christodoulou <stratis.vip@gmail.com>
2019-12-21 12:09:02 +0200

    Delete respondTexts.d.ts

--- models/respondTexts.d.ts
+++
@@ -1,7 +0,0 @@
-import GeneralRespond from './general/general-respond';
-import { IText } from './types';
-declare class RespondTexts extends GeneralRespond {
-    data: IText[];
-    constructor();
-}
-export default RespondTexts;
