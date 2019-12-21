a8342eb299086a1223af6964ee53c39f2a79ca4f
Stratis Christodoulou <stratis.vip@gmail.com>
2019-12-21 12:08:32 +0200

    Delete general-respond.d.ts

--- models/general-respond.d.ts
+++
@@ -1,9 +0,0 @@
-import { stCodes } from './types'
-export default class GeneralRespond {
-  code: number
-  data: Array<{}>
-  error?: string
-  status: stCodes
-  constructor()
-}
-export declare const checkRespond: (respond: GeneralRespond) => boolean
