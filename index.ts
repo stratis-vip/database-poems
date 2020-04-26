12539e281b2cf54cea178e192e4e5739a74cf4bd
stratis-vip <stratis.vip@gmail.com>
2020-04-26 18:30:46 +0300

    remove some exports

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import RespondTexts from './models/respondTexts'
+// import RespondTexts from './models/respondTexts'
 import { Nullable, stCodes } from './models/types'
 
 import { MALLFORMED_RESPONSE_ERROR } from './models/consts'
-import TblCategory from './models/tblCategory'
-import TblText from './models/tblText'
+// import TblCategory from './models/tblCategory'
+// import TblText from './models/tblText'
 
-export { TblCategory, TblText, RespondTexts, stCodes, MALLFORMED_RESPONSE_ERROR, Nullable }
+// export { TblCategory, TblText, RespondTexts, stCodes, MALLFORMED_RESPONSE_ERROR, Nullable }
+export { stCodes, MALLFORMED_RESPONSE_ERROR, Nullable }
